900a291ff0e7620ff035cdfe4b5c501487124295
Sergio <sergioaguadogarrido@gmail.com>
2025-02-23 21:01:57 +0100

    feat: main extension functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,33 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "auto-template-literal" is now active!');
+  const convertStringsToTemplateLiterals = (editor: vscode.TextEditor) => {
+    const document = editor.document;
+    const fullText = document.getText();
 
-  const disposable = vscode.commands.registerCommand(
-    'auto-template-literal.helloWorld',
-    () => {
-      vscode.window.showInformationMessage('Hello World from auto-template-literal!');
+    const updatedText = fullText.replace(
+      /(['"])([^'"\n]*?\$\{[^'"\n]*?[^'"\n]*?)\1/g,
+      '`$2`'
+    );
+
+    if (updatedText !== fullText) {
+      const fullRange = new vscode.Range(
+        document.positionAt(0),
+        document.positionAt(fullText.length)
+      );
+      editor.edit((editBuilder) => {
+        editBuilder.replace(fullRange, updatedText);
+      });
     }
-  );
+  };
+
+  const disposable = vscode.workspace.onDidSaveTextDocument((document) => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor || editor.document !== document) {
+      return;
+    }
+    convertStringsToTemplateLiterals(editor);
+  });
 
   context.subscriptions.push(disposable);
 }
